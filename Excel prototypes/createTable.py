40909b4e6a79fcdd12b6b50c73ade467849ed0ed
jeansung <jsung@hmc.edu>
2014-11-15 15:02:11 -0800

    more progress on Excel prototype
    Added in code to create intermediate tables

--- Excel prototypes/createTable.py
+++ Excel prototypes/createTable.py
@@ -4,44 +4,123 @@ import sys
 
 from datetime import datetime
 
-# you must program with 2 input files 
-# wrap with try catch so if they fuck it, force a rerun
+# you must program with 2 input fillesand a name for your output excel file
+# eventually change so that you run with nothing and force while loop to take
+#in  the 3 inputs
+# LIMITATIONS:
+# -  Only supports excel file through 26 values (Need to feed it more of the excel columns)
+# Maximum excel coulm XFD 
+# Formula: 	# ws.write(2, 2, xlwt.Formula("A3+B3"))
 
-# check inputs (files correct)
+EXCEL_ROW_MAPPING = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
 
- # while True:
- #        try:
- #            #file name of artists list
- #            inFileName = sys.argv[1]
-
- #            #cutoff value, inclusive
- #            cutOffValue = int(sys.argv[2])
- #            break
- #        except IndexError or TypeError:
- #            print ("Incorrect running of program. Must run as: 'python3 knewtonsolution.py Artist_lists_small.txt 50'")
- #            sys.exit()
-
+EXCEL_FILE_EXTENSION = ".xls"
+TABLE_BUFFER_ROW = 2
+TABLE_BUFFER_COL = 2
 
 def main():
-	(typeFileName, valueFileName) = properInputFiles()
+	# check inputs 
+	(typeFileName, valueFileName, outputFileName) = properInputFiles()
+
+	# variables to keep track of where I can put things next 
+	freeRow = 0
+	freeCol = 0
+
+	# create workbook and worksheet
 	wb = createWorkbook()
 	ws = createWorksheet(wb, "abcdefghijklmnop")
-	
 
-	# parseFiles()
-	
+	# parse initial table
+	(freeRow, freeCol, types, values) = createInitialTable(wb, ws, typeFileName, valueFileName,
+		 freeRow, freeCol)
+
+	# calculate input sums/ # inputs per table 
+	(freeRow, freeCol) = createSumFromDifficulty(wb, ws, types, values, freeRow, freeCol)
+	(freeRow, freeCol) = createNumberOfInputs(wb, ws, types, values, freeRow, freeCol)
+
+	# input rules 
+	# Parse rules under the assumption that they 
+	# are correct (refer to table included )
+
+
+	print freeRow
+	print freeCol
+	wb.save(outputFileName + EXCEL_FILE_EXTENSION)
+
 	return 
 
+# Create column that counts number 
+def createNumberOfInputs(wb, ws, types, values, freeRow, freeCol):
+	for type in range (0, len(types)):
+		adjIndex = type + 1
+		#Default for now 
+		startColIndex = 1
+		startCol = EXCEL_ROW_MAPPING[startColIndex]
+		endColIndex = startColIndex + len(values) - 1
+		endCol = EXCEL_ROW_MAPPING[endColIndex]
+
+
+		startColStr = str(startCol) + str(adjIndex+1)
+		endColStr = str(endCol) + str(adjIndex+1)
+
+
+		formulaStr  = "SUM(" + startColStr + ":" + endColStr  + ")"
+
+		row = adjIndex
+		col = endColIndex + TABLE_BUFFER_ROW
+		# write it in the row col
+		ws.write(row, col, xlwt.Formula(formulaStr))
+	freeCol = freeCol + TABLE_BUFFER_COL + 1
+	return (freeRow, freeCol)
+
+
+	# create # input tables
+	# for each of the rows (i.e. types) so through the number of values
+	# col start,  get letter 
+	# col end , get letter
+	# row number is index 
+	# Sum (Start: Ennd)
+
+
+
+
+
+# Create row of sums from each multiplier 
+def createSumFromDifficulty(wb, ws, types, values, freeRow, freeCol):
+	for difficulty in range(len(values)):
+		adjIndex = difficulty + 1
+
+		colLetter = EXCEL_ROW_MAPPING[adjIndex]
+		multiplierRow = 1
+		startRow = multiplierRow + 1
+		endRow = startRow + (len(types) -1)
+
+		multiplierRowStr = colLetter + str(multiplierRow)
+		startRowStr = colLetter + str(startRow)
+		endRowStr = colLetter + str(endRow)
+
+		formulaStr = "SUM(" + startRowStr + ":" + endRowStr  + ")" + "*" + multiplierRowStr
+
+		row = freeRow
+		col = adjIndex
+
+		ws.write(row, col, xlwt.Formula(formulaStr))
+
+	freeRow = freeRow + 1 
+	return (freeRow, freeCol)	
+
+
+
+# Check to see if command line arguments are correct 
 def properInputFiles():
 	try:
 		typeFileName = sys.argv[1]
 		valueFileName = sys.argv[2]
-		return (typeFileName, valueFileName)
+		outputFileName = sys.argv[3]
+		return (typeFileName, valueFileName, outputFileName)
 	except IndexError or TypeError:
-		print "Incorrect run of program. Must run as python2 createTable.py types.txt values.txt"
+		print "Incorrect run of program. Must run as python2 createTable.py types.txt values.txt outputFileName"
 		sys.exit(0) 
-
-
 
 def createWorkbook():
 	return xlwt.Workbook()
@@ -49,51 +128,22 @@ def createWorkbook():
 def createWorksheet(workbook, sheetName):
 	return workbook.add_sheet(sheetName);
 
-
-def parseFiles():
-
-	wb = xlwt.Workbook()
-	style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on',
-		num_format_str='#,##0.00')
-	style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
-
-	ws = wb.add_sheet('A Test Sheet')
-
-	ouputFileName = sys.argv[1]
-
-	# typeFileName = sys.argv[1]
-	# valueFileName = sys.argv[2]
-
-	typeFileName = "types.txt"
-	valueFileName = "values.txt"
-
+# Create initial types/ values table 
+def createInitialTable(wb, ws, typeFileName, valueFileName, freeRow, freeCol):
 	types = inputTypes(typeFileName)
-
 	values = inputValues(valueFileName)
 
 	for typeIndex in range(len (types)):
 		adjIndex = typeIndex +1
 		ws.write(adjIndex, 0, types[typeIndex])
+		freeRow = adjIndex + 1
 
 	for valueIndex in range(len (values)):
 		adjIndex = valueIndex + 1
 		ws.write(0, adjIndex, values[valueIndex])
+		freeCol = adjIndex +1 
 
-
-
-	# ws.write(2, 2, xlwt.Formula("A3+B3"))
-
-	wb.save(ouputFileName+".xls")
-
-
-
-
-def setUpWorkBook():
-
-
-
-	return 
-
+	return (freeRow, freeCol, types, values) 
 
 
 # Takes a file name as a string
@@ -103,7 +153,7 @@ def inputTypes(filename):
 	try:
 		file = codecs.open(filename)
 	except IOError:
-		print "You fucked up the file. I can't open it."
+		print "Types file incorrectly formatting or does not exist."
 		sys.exit(0)
 
 	for line in file:
@@ -119,7 +169,7 @@ def inputValues(filename):
 	try:
 		file = codecs.open(filename)
 	except IOError:
-		print "You fucked up the file. I can't open it."
+		print "Values file incorrectly formatted or does not exist."
 		sys.exit(0)
 
 	for line in file:
@@ -132,12 +182,6 @@ def inputValues(filename):
 		values.append(numValue)
 	file.close()
 	return values
-
-
-#def populateExcelWithTable():
-
-
-
 
 
 # automatically runs main method when script runs
--- Excel prototypes/makeExcelDictionary.py
+++ Excel prototypes/makeExcelDictionary.py
@@ -3,6 +3,6 @@ excelCol= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 excelColDictionary = {}
 for index in range(len(excelCol)):
 	currentLetter = excelCol[index]
-	excelColDictionary[currentLetter] = index
+	excelColDictionary[index] = currentLetter
 
 print excelColDictionary
